5fbcf48ec8324e77c79081acc56988c5b805d7b8
sebastian200188 <sebastiantabaresrozo2001@gmail.com>
2022-09-11 20:19:13 -0500

    M

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,9 +13,9 @@ const Form = ({heroku_9dab5d1d4fe89e1, setform}) => {
     //let{Nombre, Cédula, Domicilio, Ciudad, Correo, Whatsapp, Actividad} = heroku_9dab5d1d4fe89e1
 
     const handleSubmit = () => {
-        //if (heroku_9dab5d1d4fe89e1.Nombre === '' || heroku_9dab5d1d4fe89e1.Cédula === '' || heroku_9dab5d1d4fe89e1.Domicilio === '' || heroku_9dab5d1d4fe89e1.Ciudad === '' || heroku_9dab5d1d4fe89e1.Correo === '' || heroku_9dab5d1d4fe89e1.Whatsapp === '' || heroku_9dab5d1d4fe89e1.Actividad === '') {
-        //    alert("debes llenar todos el formulario")
-        //}
+        if (heroku_9dab5d1d4fe89e1.Nombre === '' || heroku_9dab5d1d4fe89e1.Cédula === '' || heroku_9dab5d1d4fe89e1.Domicilio === '' || heroku_9dab5d1d4fe89e1.Ciudad === '' || heroku_9dab5d1d4fe89e1.Correo === '' || heroku_9dab5d1d4fe89e1.Whatsapp === '' || heroku_9dab5d1d4fe89e1.Actividad === '') {
+            alert("debes llenar todos el formulario")
+        }
 
         const requestInit = {
             method: 'POST',
